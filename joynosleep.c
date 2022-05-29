ec92fef36f87ea5e85289ffcb460302082314129
ksv1986 <ksv0x07c2@gmail.com>
2022-05-29 18:40:14 +0300

    Handle joystick key presses

--- joynosleep.c
+++ joynosleep.c
@@ -2,10 +2,14 @@
 #include <systemd/sd-device.h>
 #include <systemd/sd-event.h>
 
+#include <linux/input.h>
+
 #include <assert.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <strings.h>
+#include <unistd.h>
 
 #define PROJECT_NAME "joynosleep"
 #define SAVER       "org.freedesktop.ScreenSaver"
@@ -14,13 +18,47 @@
 #define DBUS        "org.freedesktop.DBus"
 #define DBUS_PATH   "/org/freedesktop/DBus"
 
+// to keep everything simple, use static buffer for tracked joysticks.
+// there are not many games (even for arcades) that support more than 4 players,
+// so current limit is already too generous.
+#define MAX_JOYSTICKS 16
+
 #define cleanup(f) __attribute__((cleanup(f)))
 #define unused __attribute__ ((unused))
+
+typedef struct joystick {
+    sd_device *dev;
+    const char *devname;
+    const char *name;
+    sd_event_source *source;
+    uint64_t n_events;
+} joystick;
+
+static sd_bus *g_bus;
+static uint32_t g_cookie;
+
+static const uint64_t accuracy    =  60000000; //  1min
+static uint64_t g_inhibit_timeout = 600000000; // 10min
+static sd_event_source *g_timer;
+
+static joystick g_joysticks[MAX_JOYSTICKS];
+static size_t n_joysticks;
 
 static int
 log_error(int error, const char *message) {
     const int r = -error;
     fprintf(stderr, "%s: %d %s\n", message, r, strerror(r));
+    return error;
+}
+
+static int
+log_errorf(int error, const char *fmt, ...) {
+    const int r = -error;
+    va_list va;
+    va_start(va, fmt);
+    vfprintf(stderr, fmt, va);
+    va_end(va);
+    fprintf(stderr, ": %d %s\n", r, strerror(r));
     return error;
 }
 
@@ -140,8 +178,115 @@ joystick_probe(sd_device *d, const char **devname, const char **name) {
     return 1;
 }
 
+static void
+joystick_destroy(void *userdata) {
+    joystick *j = userdata;
+
+    sd_device_unref(j->dev);
+    assert((signed)n_joysticks > 0);
+    --n_joysticks;
+    const size_t n = j - g_joysticks;
+    if (n < n_joysticks) {
+        // we just freed slot in the middle. swap previous last joystick with one.
+        *j = g_joysticks[n_joysticks];
+        sd_event_source_set_userdata(j->source, j);
+    }
+}
+
+static void
+joystick_del(joystick *j) {
+    printf("-%zd/%zd: %s %s events=%" PRId64 "\n",
+        j - g_joysticks, n_joysticks, j->devname, j->name, j->n_events);
+    sd_event_source_disable_unref(j->source);
+}
+
 static int
-joystick_enumerate(void) {
+is_button_press(const struct input_event *event) {
+    return event->type == EV_KEY && event->value == 0;
+}
+
+static int
+on_joystick_read(unused sd_event_source *s, int fd,
+    unused uint32_t revents, unused void *userdata)
+{
+    int r;
+    joystick *j = userdata;
+
+    struct input_event event[1];
+    r = read(fd, event, sizeof(event));
+    if (r != sizeof(event))
+        return log_errorf(-errno, "%s %s read failed", j->name, j->devname);
+
+    ++j->n_events;
+    if (!is_button_press(event))
+        return 0;
+
+    if (!g_cookie) {
+        r = saver_inhibit(g_bus, j->name, &g_cookie);
+        if (r < 0)
+            return r;
+    }
+
+    r = sd_event_source_set_time_relative(g_timer, g_inhibit_timeout);
+    if (r < 0)
+        return log_error(r, "Failed to reset the timer");
+
+    r = sd_event_source_set_enabled(g_timer, SD_EVENT_ONESHOT);
+    if (r < 0)
+        return log_error(r, "Failed to enable the timer");
+
+    return 0;
+}
+
+static int
+joystick_add(sd_event *ev, sd_device *d, const char *devname, const char *name) {
+    int r;
+
+    assert(n_joysticks < MAX_JOYSTICKS);
+
+    int fd = open(devname, O_RDONLY|O_CLOEXEC|O_NONBLOCK);
+    if (fd < 0)
+        return log_errorf(-errno, "Failed to open %s device %s", name, devname);
+
+    joystick *j = &g_joysticks[n_joysticks];
+    r = sd_event_add_io(ev, &j->source, fd, EPOLLIN, on_joystick_read, j);
+    if (r < 0) {
+        close(fd);
+        return log_errorf(r, "Failed to add %s %s to event loop", name, devname);
+    }
+
+    r = sd_event_source_set_io_fd_own(j->source, 1);
+    assert(r >= 0);
+
+    r = sd_event_source_set_destroy_callback(j->source, joystick_destroy);
+    assert(r >= 0);
+
+    j->dev = sd_device_ref(d);
+    j->devname = devname;
+    j->name = name;
+    j->n_events = 0;
+
+    printf("+%zd: %s %s\n", n_joysticks, devname, name);
+    ++n_joysticks;
+    return 0;
+}
+
+static void
+joystick_del_all(void) {
+    while (n_joysticks) {
+        joystick *j = &g_joysticks[n_joysticks - 1];
+        joystick_del(j);
+    }
+}
+
+static int
+joystick_exit(unused sd_event_source *s, unused void *userdata) {
+    joystick_del_all();
+    return 0;
+}
+
+static int
+joystick_enumerate(sd_event *ev) {
     int r;
 
     cleanup(sd_device_enumerator_unrefp) sd_device_enumerator *e;
@@ -166,27 +311,18 @@ joystick_enumerate(void) {
             continue;
 
         ++joysticks;
-        printf("%s %s\n", devname, name);
+        joystick_add(ev, d, devname, name);
     }
 
-    printf("Found %d inputs, %d joysticks\n", inputs, joysticks);
+    printf("Found %d inputs, %d joysticks, %zd tracked\n",
+        inputs, joysticks, n_joysticks);
     return 0;
 }
 
 static int
 on_screen_saver_appeared(sd_bus *bus) {
-    int r;
-
-    r = joystick_enumerate();
-    if (r < 0)
-        return r;
-
-    uint32_t cookie = 0;
-    r = saver_inhibit(bus, "test", &cookie);
-    if (r < 0)
-        return r;
-
-    return saver_uninhibit(bus, &cookie);
+    sd_event *ev = sd_bus_get_event(bus);
+    return joystick_enumerate(ev);
 }
 
 static int
@@ -246,6 +382,31 @@ start(sd_bus *bus) {
 }
 
 static int
+on_timer(sd_event_source *s, unused uint64_t usec, void *userdata) {
+    sd_bus *bus = userdata;
+    assert(g_bus == bus);
+    assert(g_timer == s);
+    assert(g_cookie);
+
+    return saver_uninhibit(bus, &g_cookie);
+}
+
+static int
+timer_init(sd_event *ev, sd_bus *bus) {
+    int r;
+
+    r = sd_event_add_time_relative(ev, &g_timer, CLOCK_MONOTONIC,
+        g_inhibit_timeout, accuracy, on_timer, bus);
+    if (r < 0)
+        return log_error(r, "Failed to initialize timerfd");
+
+    r = sd_event_source_set_enabled(g_timer, SD_EVENT_OFF);
+    assert(r >= 0);
+
+    return 0;
+}
+
+static int
 bus_fini(unused sd_event_source *s, void *userdata) {
     sd_bus *bus = userdata;
     sd_bus_unref(bus);
@@ -268,6 +429,13 @@ bus_init(sd_event_source *s, unused void *userdata) {
     r = sd_bus_attach_event(bus, ev, SD_EVENT_PRIORITY_NORMAL);
     if (r < 0)
         return log_error(r, "Failed to attach D-Bus to event loop");
+
+    r = timer_init(ev, bus);
+    if (r < 0)
+        return r;
+
+    assert(!g_bus);
+    g_bus = bus;
 
     start(bus);
 
@@ -306,6 +474,9 @@ main(int argc, char **argv) {
         return log_error(r, "Failed to allocate event loop");
 
     signal_init(ev);
+
+    r = sd_event_add_exit(ev, NULL, joystick_exit, NULL);
+    assert(r >= 0);
 
     r = sd_event_add_defer(ev, NULL, bus_init, NULL);
     if (r < 0)
